f891365d64c61766c00de644bb70f7c1a629b0ef
Ayunda <ayundahamzah@gmail.com>
2017-11-13 16:18:08 +0700

    done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,16 @@
 function to_roman (num) {
-  // your implementation code here
+  //Buat komparasi yang unik dari roman dan arabic numerals
+  var arabic = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+  var roman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+  let result =""
+  for(i = 0; i < arabic.length; i++){
+    while(num>=arabic[i]){
+      result+=roman[i]
+      //agar looping tidak eve
+      num-=arabic[i]
+    }
+  }
+return result;
 }
 
 // Drive code
