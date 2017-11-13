70b64ea84563830106cacec6c0116841c52df694
lenz77 <rickylenzun77@gmail.com>
2017-11-13 22:12:30 +0700

    Mission Completed

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,15 @@
-function to_roman (num) {
+function to_roman(num) {
   // your implementation code here
+  var hasil = ""
+  var angka = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  for (var i = 0; i <= angka.length; i++) {
+    while (num % angka[i] < num) {
+      hasil += roman[i];
+      num -= angka[i];
+    }
+  }
+  return hasil;
 }
 
 // Drive code
