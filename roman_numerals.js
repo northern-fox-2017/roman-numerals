ee499af1661b729f6e817517594753d2d716b44e
Andrey Amrulloh <andrey.amrulloh@gmail.com>
2017-11-13 17:19:40 +0700

    Roman Numerals Done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,60 @@
 function to_roman (num) {
-  // your implementation code here
+  var romanObj = {
+	1:'I',
+	5:'V',
+	10:'X',
+	50:'L',
+	100:'C',
+	500:'D',
+	1000:'M'
+	}
+	var arr = [];
+	while(num >= 1000){
+	  arr.push(romanObj[1000]);
+	  num = num - 1000;
+	}
+	while(num >= 500){
+	  arr.push(romanObj[500]);
+	  num = num - 500;
+	}
+	while(num >= 400){
+	  arr.push('CD');
+	  num = num - 400;
+	}
+	while(num >= 100){
+	  arr.push(romanObj[100]);
+	  num = num - 100;
+	}
+	while(num >= 50){
+	  arr.push(romanObj[50])
+	  num = num - 50;
+	}
+	while(num > 40){
+	  arr.push('XL');
+	  num = num - 40;
+	}
+	while(num >= 10){
+	  arr.push(romanObj[10]);
+	  num = num - 10;
+	}
+	while(num >= 9){
+	  arr.push('IX');
+	  num = num - 9;
+	}
+	while(num >= 5 ){
+	  arr.push(romanObj[5]);
+	  num = num - 5;
+	}
+	while(num >= 4){
+	  arr.push('IV');
+	  num = num - 4;
+	}
+	while(num >= 1){
+	  arr.push(romanObj[1]);
+	  num = num - 1;
+	}
+	join = arr.join('');
+	return join;
 }
 
 // Drive code
