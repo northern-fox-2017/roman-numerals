e4b5ba4077983509d03ce9de13547048f5614d95
rezaalfarabi <anangreza18@gmail.com>
2017-11-13 16:09:17 +0700

    release 0 fix identasi

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,7 +1,7 @@
 function to_roman (num) {
   // your implementation code here
   let arrRoman = ['M','D','CD','C','XC','L','XL','X','IX','V','IV','I']
-	let arrLatin = [1000,500,400,100,90,50,40,10,9,5,4,1]
+  let arrLatin = [1000,500,400,100,90,50,40,10,9,5,4,1]
   let hasil = ''
 
   for (var i = 0; i < arrRoman.length; i++) {
