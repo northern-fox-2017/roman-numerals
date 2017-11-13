10e2353a682bf26ea8296756aab193a62938cf7e
Budi <cbudiman1000@gmail.com>
2017-11-13 22:41:33 +0700

    done

---
+++ latihan2.js
@@ -0,0 +1,5 @@
+var roman = ["I", "IV", "V", "VI", "X", "XL", "L", "LX", "XC", "C", "CD", "D", "DC", "M"];
+var angka = [1,4,5,6,9,10,40,50,60,90,100,400,500,600,1000];
+
+console.log(roman.length);
+console.log(angka.length);
--- roman_numerals.js
+++ roman_numerals.js
@@ -1,13 +1,25 @@
 function to_roman (num) {
-  // your implementation code here
+  var roman = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "M"];
+  var angka = [1,4,5,9,10,40,50,90,100,400,500,1000];
+  var result = "";
+
+  for (var i = angka.length-1; i >= 0; i--) {
+    while (num >= angka[i]) {
+      result += roman[i];
+      num -= angka[i];
+    }
+  } 
+  return result;
 }
 
+
+
 // Drive code
-console.log('My totally sweet testing script for new roman\n')
-console.log('input | expected | actual')
-console.log('——————|——————————|———————')
-console.log('4     | IV       | ', to_roman(4))
-console.log('9     | IX       | ', to_roman(9))
-console.log('13    | XIII     | ', to_roman(13))
-console.log('1453  | MCDLIII  | ', to_roman(1453))
-console.log('1646  | MDCXLVI  | ', to_roman(1646))
+console.log('My totally sweet testing script for new roman\n');
+console.log('input | expected | actual');
+console.log('——————|——————————|———————');
+console.log('4     | IV       | ', to_roman(4));
+console.log('9     | IX       | ', to_roman(9));
+console.log('13    | XIII     | ', to_roman(13));
+console.log('1453  | MCDLIII  | ', to_roman(1453));
+console.log('1646  | MDCXLVI  | ', to_roman(1646));
