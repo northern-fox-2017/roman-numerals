56e176ca29b6a416d5178df162ee6b79598ab2d0
name": null <email": null>
2017-11-13 16:36:50 +0700

    Menambah isi function

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,61 @@
 function to_roman (num) {
-  // your implementation code here
+  var result = '';
+  while (num>0) {
+    if (num >= 1000) {
+      num -= 1000;
+      result += 'M';
+    }
+    if (num == 900) {
+      num -= 900;
+      result += 'CM';
+    }
+    if (num>=500) {
+      num -= 500;
+      result += 'D';
+    }
+    if (num >= 400) {
+      num -= 400;
+      result += 'CD'
+    }
+    if (num>=100 ) {
+      num -= 100;
+      result += 'C';
+    }
+    if (num >= 90) {
+      num -= 90;
+      result += 'XC';
+    }
+    if (num >= 50) {
+      num -= 50
+      result += 'L';
+    }
+    if (num >= 40) {
+      num -= 40;
+      result += 'XL';
+    }
+    if (num >= 10) {
+      num -= 10;
+      result += 'X';
+    }
+    if (num >= 9) {
+      num -= 9;
+      result += 'IX';
+    }
+    if (num >= 5) {
+      num -= 5;
+      result += 'V';
+    }
+    if (num >= 4) {
+      num-=4;
+      result+= 'IV';
+    }
+    if (num < 4 && num > 0) {
+      num -= 1;
+      result += 'I';
+    }
+  }
+
+  return result;
 }
 
 // Drive code
