a68ed576134b6e0403cf68e8c2f9bce2fec97820
agungpangestu <agung_pangestu83@ymail.com>
2017-11-13 21:58:11 +0700

    Done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,20 @@
 function to_roman (num) {
   // your implementation code here
+
+
+let kamusRoman = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}
+let roman = ''
+let i;
+  for ( i in kamusRoman ) {
+    while ( num >= kamusRoman[i] ) {
+      roman += i;
+      num -= kamusRoman[i];
+    }
+  }
+  return roman;
+
+
+
 }
 
 // Drive code
